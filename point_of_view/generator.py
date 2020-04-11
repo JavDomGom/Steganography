78c3bea952ac79a1c140543a61d21aade166912a
JavierDominguezGomez <jdomgo3@gmail.com>
2020-04-11 22:02:40 +0200

    Add data.csv file and update generator.py.

--- point_of_view/generator.py
+++ point_of_view/generator.py
@@ -1,4 +1,16 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.scatter(3, 3, s=100)
+# Import dataset
+dataset = pd.read_csv('data.csv', delimiter=',', encoding='utf-8')
+
+for i in dataset.itertuples():
+    x = dataset.at[i.Index, 'x']
+    y = dataset.at[i.Index, 'y']
+    s = dataset.at[i.Index, 's']
+    plt.scatter(x, y, s=s, color='black')
+
+plt.title('Point of view')
+plt.xlabel('X')
+plt.ylabel('Y')
 plt.show()
